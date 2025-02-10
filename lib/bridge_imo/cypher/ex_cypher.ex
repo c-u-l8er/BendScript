82f8f2fe0626c3e5f9da23d7a84816c6b35a4013
Travis Burandt <travis.burandt@gmail.com>
2025-02-10 11:24:14 -0600

    cypher spaceships 2

--- lib/bridge_imo/cypher/ex_cypher.ex
+++ lib/bridge_imo/cypher/ex_cypher.ex
@@ -2,30 +2,44 @@ defmodule ExCypher do
   require Logger
 
   defmacro cypher(do: block) do
+    clauses =
+      case block do
+        {:__block__, [], exprs} -> exprs
+        expr -> [expr]
+      end
+
     quote do
-      Logger.debug("Building Cypher query from block: #{inspect(unquote(block))}")
-      ExCypher.Builder.build(unquote(block))
+      clauses =
+        unquote(clauses)
+        |> Enum.map(fn clause -> clause end)
+        |> List.flatten()
+        |> Enum.reject(&is_nil/1)
+        |> Enum.reject(&(&1 == ""))
+
+      query = Enum.join(clauses, " ")
+      Logger.debug("Generated query string: #{inspect(query)}")
+      query
     end
   end
 
   def match(pattern) do
     Logger.debug("Building MATCH clause with pattern: #{inspect(pattern)}")
-    ["MATCH", pattern]
+    "MATCH " <> pattern
   end
 
   def where(condition) do
     Logger.debug("Building WHERE clause with condition: #{inspect(condition)}")
-    ["WHERE", condition]
+    "WHERE " <> condition
   end
 
   def create(pattern) do
     Logger.debug("Building CREATE clause with pattern: #{inspect(pattern)}")
-    ["CREATE", pattern]
+    "CREATE " <> pattern
   end
 
   def return(var) when is_atom(var) do
     Logger.debug("Building RETURN clause for variable: #{inspect(var)}")
-    ["RETURN", "#{var}"]
+    "RETURN " <> Atom.to_string(var)
   end
 
   def node(var, labels, properties \\ nil) do
@@ -59,69 +73,4 @@ defmodule ExCypher do
 
   defp format_value(value) when is_binary(value), do: "'#{value}'"
   defp format_value(value), do: to_string(value)
-
-  defmodule Builder do
-    require Logger
-
-    def build(ast) do
-      Logger.debug("Building query from AST: #{inspect(ast)}")
-      {clauses, state} = traverse(ast, %{params: [], returns: []})
-      Logger.debug("Traversal result - clauses: #{inspect(clauses)}, state: #{inspect(state)}")
-
-      result =
-        clauses
-        |> List.flatten()
-        |> tap(&Logger.debug("After flatten: #{inspect(&1)}"))
-        |> Enum.reject(&is_nil/1)
-        |> tap(&Logger.debug("After nil rejection: #{inspect(&1)}"))
-        |> Enum.reject(&(&1 == ""))
-        |> tap(&Logger.debug("After empty string rejection: #{inspect(&1)}"))
-        |> Enum.join(" ")
-
-      Logger.debug("Final built query: #{result}")
-      result
-    end
-
-    defp traverse({:__block__, _, statements}, state) do
-      Logger.debug("Traversing block with statements: #{inspect(statements)}")
-
-      {clauses, final_state} =
-        Enum.reduce(statements, {[], state}, fn stmt, {acc_clauses, acc_state} ->
-          Logger.debug("Processing statement: #{inspect(stmt)}")
-          {clause, new_state} = traverse(stmt, acc_state)
-
-          Logger.debug(
-            "Statement result - clause: #{inspect(clause)}, state: #{inspect(new_state)}"
-          )
-
-          {acc_clauses ++ [clause], new_state}
-        end)
-
-      Logger.debug(
-        "Block traversal complete - clauses: #{inspect(clauses)}, state: #{inspect(final_state)}"
-      )
-
-      {clauses, final_state}
-    end
-
-    defp traverse({func, _, [arg]} = expr, state)
-         when func in [:match, :where, :create, :return] do
-      Logger.debug("Traversing function call: #{inspect(expr)}")
-      result = apply(ExCypher, func, [arg])
-      Logger.debug("Function call result: #{inspect(result)}")
-      {result, state}
-    end
-
-    defp traverse(value, state) when is_binary(value) or is_list(value) do
-      Logger.debug("Traversing direct value: #{inspect(value)}")
-      {value, state}
-    end
-
-    defp traverse(other, state) do
-      Logger.debug("Traversing other value: #{inspect(other)}")
-      result = to_string(other)
-      Logger.debug("Converted to string: #{inspect(result)}")
-      {result, state}
-    end
-  end
 end
